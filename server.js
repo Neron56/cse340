9013cc3d7ae8c54c1bf98bcd3ec9c0d5a6ca9c3e
Neron56 <Michaelshaw20004@gmail.com>
2026-09-10 19:47:34 -0600

    Week 2 group activity

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import { getAllOrganizations } from './src/models/organizations.js';
+import { getAllServiceProjects } from './src/models/projects.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -44,13 +45,18 @@ app.get('/organizations', async (req, res) => {
 });
 
 app.get('/projects', async (req, res) => {
+    const service_projects = await getAllServiceProjects();
+    service_projects.forEach(project => {
+      console.log(project.name);
+    })
     const title = 'Service Projects';
-    res.render('projects', { title });
+    res.render('projects', { title, service_projects });
 });
 
 app.get('/categories', async (req, res) => {
+    const categories = await getAllCategories();
     const title = 'Service Projects';
-    res.render('categories', { title });
+    res.render('categories', { title, categories });
 });
 
 app.get('/', (req, res) => {
---
+++ src/models/categories.js
@@ -0,0 +1,15 @@
+import db from './db.js'
+
+const getAllCategories = async() => {
+    const query = `
+        SELECT name
+        FROM public.categories;
+        `;
+
+    const result = await db.query(query);
+
+    return result.rows;
+}
+
+export {getAllCategories}  
+
